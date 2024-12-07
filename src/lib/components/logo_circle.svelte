3a8c6e8d8520b455d9a7825be94a1729b35a7ca2
triesap <137732411+triesap@users.noreply.github.com>
2024-12-07 22:32:33 +0000

    Edit logo circle. Add/edit locales.

--- src/lib/components/logo_circle.svelte
+++ src/lib/components/logo_circle.svelte
@@ -8,15 +8,22 @@
     class={`relative flex flex-col h-[196px] w-full justify-center items-center`}
 >
     <div
-        class={`flex flex-row h-32 w-32 justify-center items-center bg-layer-2-surface rounded-full`}
+        class={`relative flex flex-row h-36 w-36 justify-center items-center bg-layer-2-surface rounded-full`}
     >
-        <p class={`font-sans font-[900] text-4xl text-layer-0-glyph`}>
-            {"»`,-"}
+        <p
+            class={`font-sans font-[900] text-6xl text-layer-0-glyph -tracking-[0.4rem] -translate-x-[6px]`}
+        >
+            {"»`,"}
+        </p>
+        <p
+            class={`font-sans font-[900] text-6xl text-layer-0-glyph translate-x-[8px]`}
+        >
+            {"-"}
         </p>
     </div>
     {#each array as char, index}
         <div
-            class={`char font-sans text-layer-0-glyph text-center uppercase`}
+            class={`char font-sans text-layer-0-glyph/60 text-[0.8rem] text-center uppercase`}
             style="--angle: {`${(1 / array.length) * index + 0.18}turn`}"
         >
             {char}
